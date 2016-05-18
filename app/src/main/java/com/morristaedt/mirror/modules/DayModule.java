3bf1d4813e09858cd9bb5199c3cd94a62e8990fe
tostadora <tostadora@gmail.com>
2016-05-18 15:51:53 +0000

    Removed TimeModule. DayModule will have its functionality

--- app/src/main/java/com/morristaedt/mirror/modules/DayModule.java
+++ app/src/main/java/com/morristaedt/mirror/modules/DayModule.java
@@ -1,7 +1,11 @@
 package com.morristaedt.mirror.modules;
 
+import android.content.res.Resources;
+import android.support.annotation.NonNull;
 import android.text.Html;
 import android.text.Spanned;
+
+import com.morristaedt.mirror.R;
 
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
@@ -13,10 +17,38 @@ import java.util.Locale;
 public class DayModule {
 
     public static Spanned getDay() {
-        SimpleDateFormat formatDayOfMonth = new SimpleDateFormat("EEEE", Locale.US);
+        SimpleDateFormat formatDayOfMonth = new SimpleDateFormat("EEEE");
+        SimpleDateFormat month = new SimpleDateFormat("MMMM");
+
         Calendar now = Calendar.getInstance();
         int dayOfMonth = now.get(Calendar.DAY_OF_MONTH);
-        return Html.fromHtml(formatDayOfMonth.format(now.getTime()) + " the " + dayOfMonth + "<sup><small>" + getDayOfMonthSuffix(dayOfMonth) + "</small></sup>");
+
+        if (Locale.getDefault() == Locale.US) {
+            return Html.fromHtml(formatDayOfMonth.format(now.getTime()) + " the " + dayOfMonth + "<sup><small>" + getDayOfMonthSuffix(dayOfMonth) + "</small></sup>");
+        }
+        else {
+            return Html.fromHtml(formatDayOfMonth.format(now.getTime()) + ", " + dayOfMonth + " " + month.format(now.getTime()));
+        }
+    }
+
+    @NonNull
+    public static String getTimeOfDayWelcome(@NonNull Resources resources) {
+        Calendar now = Calendar.getInstance();
+        int hour = now.get(Calendar.HOUR_OF_DAY);
+        int timeRes;
+        if (hour < 4) {
+            timeRes = R.string.late;
+        } else if (hour < 12) {
+            timeRes = R.string.good_morning;
+        } else if (hour < 17) { // 5pm
+            timeRes = R.string.good_afternoon;
+        } else if (hour < 22) { // 10pm
+            timeRes = R.string.good_evening;
+        } else { // 10pm - midnight is bedtime
+            timeRes = R.string.bedtime;
+        }
+
+        return resources.getString(timeRes, resources.getString(R.string.owners));
     }
 
     private static String getDayOfMonthSuffix(final int n) {
